c92c46ace007514dda066b30631e828db55c5211
sprin <sprin>
2013-07-14 19:46:15 -0700

    Allow value column to be specified in graph config

--- src/qgr-graph-barchart.js
+++ src/qgr-graph-barchart.js
@@ -15,16 +15,18 @@ function ($, _, Backbone, d3) {
     t.config = t.options.graph_config;
     t.container = t.options.container;
     t.raw_data = t.options.raw_data;
+    t.label_col = t.config.label;
+    t.col = t.config.col;
 
     // Transform raw data to format needed by D3.
-    t.data = t.map_raw_data(t.raw_data, t.config.label)
+    t.data = t.map_raw_data(t.raw_data);
     t.init_labels = _.pluck(t.data, 'label')
 
     var width = $(t.container).width(),
       height = $(t.container).height();
 
     t.x = d3.scale.linear()
-        .domain([0, d3.max(t.data, function(d) { return d.val; })])
+        .domain([0, d3.max(t.data, function(d) { return d[t.col]; })])
         .range(["0px", 0.8 * width + 'px']),
 
     t.chart = d3.select(t.container).append("div")
@@ -36,8 +38,8 @@ function ($, _, Backbone, d3) {
     t.chart.selectAll("div")
       .data(t.data)
       .enter().append("div")
-      .style("width", function(d) { return t.x(d.val); })
-      .text(function(d) { return d.label + ': ' + d.val; });
+      .style("width", function(d) { return t.x(d[t.col]); })
+      .text(function(d) { return d.label + ': ' + d[t.col]; });
 
   },
 
@@ -47,7 +49,7 @@ function ($, _, Backbone, d3) {
 
     t.raw_data = raw_data;
 
-    t.data = t.map_raw_data(t.raw_data, t.config.label)
+    t.data = t.map_raw_data(t.raw_data);
     t.data = t.order_by_init_labels(t.data, t.init_labels);
 
     var bar = t.chart.selectAll("div")
@@ -55,38 +57,38 @@ function ($, _, Backbone, d3) {
 
     bar
       .style("visibility", function(d) {
-        if (d.val !== 0) {
+        if (d[t.col] !== 0) {
           return 'visible';
         } else if (d3.select(this).style('visibility') == 'hidden') {
           return 'hidden';
         }
       })
       .text(function(d) {
-        if (d.val !== 0) {
-          return d.label + ': ' + d.val;
+        if (d[t.col] !== 0) {
+          return d.label + ': ' + d[t.col];
         } else {
           return '';
         }
       })
       .transition()
       .style("width", function(d) {
-        return t.x(d.val); })
+        return t.x(d[t.col]); })
       .transition()
       // Hide bars that have zero val
       .style("visibility", function(d) {
-        if (d.val === 0) {
+        if (d[t.col] === 0) {
           return 'hidden'
         }
       })
 
   },
 
-  map_raw_data: function(raw_data, label_col) {
+  map_raw_data: function(raw_data) {
+    var t = this;
     return _.map(raw_data, function(row) {
-      return {
-        val: row.val,
-        label: row[label_col]
-      };
+      var mapped = { label: row[t.label_col] };
+      mapped[t.col] = row[t.col];
+      return mapped;
     });
   },
 
